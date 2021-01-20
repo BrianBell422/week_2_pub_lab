19fefeddc5f4fffb33ad0a88dce10989247ce92b
rabisse <rabisse@gmail.com>
2021-01-20 11:58:27 +0000

    lunchtime

--- src/pub.py
+++ src/pub.py
@@ -1,9 +1,24 @@
 class Pub:
-    def __init__(self, name, cash, drinks):
+    def __init__(self, name, till, drinks):
         self.name = name
-        self.cash = cash
+        self.till = till
         self.drinks = drinks
 
     def number_of_drinks(self):
         return len(self.drinks)
+
+#Customer gives drink name
+    def find_drink(self, drink_name):
+        for drink in self.drinks:
+            if self.drinks.name == drink_name:
+                return drink
+
+#Pub gets drink price
+
+#Pub checks customer can afford drink
+
+#Pub removes money from cust wallet
+
+#Pub adds money to till
+
 
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -6,15 +6,21 @@ from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        drinks = ["beer", "whiskey"]
+        drink_1 = Drink("beer", 5, 1)
+        drink_2 = Drink("whiskey", 10, 3)
+        drinks = [drink_1, drink_2]
         self.pub = Pub("The Prancing Pony", 100, drinks)
     
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
 
-    def test_pub_cash(self):
-        self.assertEqual(100, self.pub.cash)
+    def test_pub_till(self):
+        self.assertEqual(100, self.pub.till)
 
     def test_pub_number_of_drinks(self):
         self.assertEqual(2, self.pub.number_of_drinks())
+
+    def test_find_drink(self, drink_name):
+        self.pub.find_drink("beer")
+        self.assertEqual(drink_1, drink_1)
 
